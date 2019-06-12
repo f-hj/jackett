e88da0c05e24c1db7b14d57af1406b449028a11e
Dante Issaias <dante.issaias@gmail.com>
2019-06-12 17:45:56 +0100

    Format code
    Signed-off-by: Dante Issaias <dante.issaias@gmail.com>

--- src/services/jackett.service.ts
+++ src/services/jackett.service.ts
@@ -6,8 +6,10 @@ export class JackettService {
   constructor(private host: string, private apiKey: string) {}
 
   public async search(query: string, categories?: number[]): Promise<JackettResponse> {
-    const url = `${this.host}/api/v2.0/indexers/all/results?apikey=${this.apiKey}&Query=${encodeURIComponent(query)}` +
-                `${categories ? '&Category[]=' + categories.join('&Category[]=') : ''}`;
+    const url =
+      `${this.host}/api/v2.0/indexers/all/results?apikey=${this.apiKey}&Query=${encodeURIComponent(query)}` +
+      `${categories ? '&Category[]=' + categories.join('&Category[]=') : ''}`;
+
     return request({
       url,
       json: true,

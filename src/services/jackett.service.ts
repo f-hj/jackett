83c0458c4e064912e5ca4aec93a4882bb2f0ec92
Dante Issaias <dante.issaias@gmail.com>
2019-06-16 18:07:47 +0100

    Closes #3
    Signed-off-by: Dante Issaias <dante.issaias@gmail.com>

--- src/services/jackett.service.ts
+++ src/services/jackett.service.ts
@@ -5,6 +5,49 @@ import { JackettIndexerDetails, JackettResponse, JackettResult } from '../respon
 import { xml2js } from 'xml-js';
 
 export class JackettService {
+  public Categories = {
+    Console: 1000,
+    ConsoleWii: 1030,
+    ConsoleXBox: 1040,
+    ConsoleXBox360: 1050,
+    ConsolePS3: 1080,
+    ConsoleOther: 1090,
+    ConsolePS4: 1180,
+    Movies: 2000,
+    MoviesOther: 2020,
+    MoviesSD: 2030,
+    MoviesHD: 2040,
+    Movies3D: 2050,
+    MoviesBluRay: 2060,
+    MoviesDVD: 2070,
+    Audio: 3000,
+    AudioMP3: 3010,
+    AudioVideo: 3020,
+    AudioAudiobook: 3030,
+    AudioLossless: 3040,
+    AudioOther: 2050,
+    PC: 4000,
+    PCISO: 4020,
+    PCMac: 4030,
+    PCPhoneOther: 4040,
+    PCGames: 4050,
+    PCPhoneIOS: 4060,
+    PCPhoneAndroid: 4070,
+    TV: 5000,
+    TVSD: 5030,
+    TVHD: 5040,
+    TVOther: 5050,
+    XXX: 6000,
+    XXXDVD: 6010,
+    XXXOther: 6050,
+    XXXImageset: 6060,
+    Other: 7000,
+    Books: 8000,
+    BooksEBook: 8010,
+    BooksComics: 8020,
+    BooksOther: 8050,
+  };
+
   constructor(private host: string, private apiKey: string) {}
 
   /**
